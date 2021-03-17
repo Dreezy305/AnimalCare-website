50f7bc0c615a1a42c9751be96a7e4e5d8b4a784f
Dreezy305 <bankoleidris@gmail.com>
2021-03-17 23:54:29 +0000

    initial commit  m Initial commit
    :wq

---
+++ nav.js
@@ -0,0 +1,8 @@
+const oranges = ["orange", "orange", "mango", "jellybean"];
+const apples = ["just one apple"];
+oranges.forEach((fruit) => {
+  console.count(fruit);
+});
+apples.forEach((fruit) => {
+  console.count(fruit);
+});
